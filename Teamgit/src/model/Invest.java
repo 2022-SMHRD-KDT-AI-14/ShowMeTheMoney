8b17e3b115547795003ad312bd6fa06b34b320b4
smhrd <smhrd@220.93.229.207>
2022-05-31 17:33:39 +0900

    DAY0531 게임 설계 정자연

---
+++ Teamgit/src/model/Invest.java
@@ -0,0 +1,347 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Invest {
+	Connection conn; // DB 연결을 위한 객체
+	PreparedStatement psmt; // SQL문을 실행하는 객체
+	int rs; // Query의 결과값을 받아오는 객체
+
+	public void investhigh() {
+		Random r = new Random();
+		Scanner sc = new Scanner(System.in);
+		int num0 = 1000000;
+
+		int cnt = 1;
+		int sum = 0;
+		int num2 = 0;
+		int num3 = 1;
+
+		String url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe"; // DB주소
+		String db_id = "campus_e_0516_2"; // db_id
+		String db_pw = "smhrd2"; // db_pw
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			try {
+				conn = DriverManager.getConnection(url, db_id, db_pw);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// 이익률 140% 손해율 -70%
+
+		int num1 = (num0 / 100) * (r.nextInt(210) - 70);
+
+		while (cnt <= 5) {
+			// 알고리즘
+
+			num1 = (num0 / 100) * (r.nextInt(210) - 70);
+			if (cnt == 2 || cnt == 4) {
+				System.out.println("정보를 구매하시겠습니까?");
+				System.out.println(" [1] YES(10만원 차감)  [2] NO");
+				int buy = sc.nextInt();
+				if (buy == 1) {
+					num0 = num0 - 100000;
+				}
+			}
+
+			System.out.println(cnt + ". 투자할 곳을 선택하세요!");
+			System.out.println("[1] 삼성전자 [2] 셀트리온 [3] 카카오 [4] 아무것도 선택하지 않는다(원금유지)");
+			int sel = sc.nextInt();
+			if (sel == 1) {
+				if (cnt == 2) {
+					num1 = ((num0 / 100) * 55);
+				}
+				if (cnt == 4) {
+					num1 = (num0 / 100) * 120;
+				}
+				num0 = num0 + num1;
+				System.out.println("1년 투자 손익액 : " + num1);
+				System.out.println("잔액은 " + num0 + "입니다.");
+
+			} else if (sel == 2) {
+				if (cnt == 2) {
+					num1 = -((num0 / 100) * 35);
+				}
+				if (cnt == 4) {
+					num1 = -(num0 / 100) * 70;
+				}
+
+				num0 = num0 + num1;
+				System.out.println("1년 투자 손익액 : " + num1);
+				System.out.println("잔액은 " + num0 + "원 입니다.");
+
+			} else if (sel == 3) {
+				if (cnt == 2) {
+					num1 = -(num0 / 100) * 120;
+				}
+				if (cnt == 4) {
+					num1 = (num0 / 100) * 130;
+				}
+				num0 = num0 + num1;
+				System.out.println("1년 투자 손익액 : " + num1);
+				System.out.println("잔액은 " + num0 + "원 입니다.");
+
+			} else if (sel == 4) {
+				num0 = num0 + sum;
+				System.out.println("1년 투자 손익액 : " + num1);
+				System.out.println("잔액은 " + num0 + "원 입니다.");
+
+			} else {
+				System.out.println("잘못입력하셨습니다.");
+
+			}
+			cnt++;
+			if (cnt == 6) {
+				String sql = "update user_info set SEEDMONEY = ";
+
+				try {
+					psmt = conn.prepareStatement(sql);
+					rs = psmt.executeUpdate();
+					System.out.println("종료되었습니다.");
+					Rank rank = new Rank();
+					rank.gamerank();
+
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			} else {
+				System.out.println("계속하시겠습니까?");
+				System.out.println("[1] Yes [2] NO");
+				num2 = sc.nextInt();
+			}
+			if (num2 == 2) {
+				System.out.println("잔액은 " + num0 + "원 입니다.");
+				System.out.println("종료되었습니다!");
+				break;
+			}
+
+		}
+
+	}
+
+	public void investmid() {
+		String url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe"; // DB주소
+		String db_id = "campus_e_0516_2"; // db_id
+		String db_pw = "smhrd2"; // db_pw
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			try {
+				conn = DriverManager.getConnection(url, db_id, db_pw);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Random r = new Random();
+		Scanner sc = new Scanner(System.in);
+		int num0 = 1000000;
+
+		int cnt = 1;
+		int sum = 0;
+		int num2 = 0;
+
+		// 이익률 100% 손해율 -55%
+
+		int num1 = ((num0 / 100) * r.nextInt(155) - 55);
+		while (cnt <= 5) {
+			// 알고리즘
+			if (cnt == 2 || cnt == 4) {
+				System.out.println("정보를 구매하시겠습니까?");
+				System.out.println(" [1] YES(10만원 차감)  [2] NO");
+				int buy = sc.nextInt();
+				if (buy == 1) {
+					num0 = num0 - 100000;
+				}
+			}
+
+			num1 = ((num0 / 100) * r.nextInt(155) - 55);
+			System.out.println(cnt + ". 투자할 곳을 선택하세요!");
+			System.out.println("[1] 삼성전자 [2] 셀트리온 [3] 카카오 [4] 아무것도 선택하지 않는다(원금유지)");
+			int sel = sc.nextInt();
+			if (sel == 1) {
+				if (cnt == 2) {
+					num1 = ((num0 / 100) * 60);
+				}
+				if (cnt == 4) {
+					num1 = -(num0 / 100) * 40;
+				}
+				num0 = num0 + num1;
+				System.out.println("1년 투자 손익액 : " + num1);
+				System.out.println("잔액은 " + num0 + "입니다.");
+
+			} else if (sel == 2) {
+				if (cnt == 2) {
+					num1 = -((num0 / 100) * 35);
+				}
+				if (cnt == 4) {
+					num1 = -(num0 / 100) * 45;
+				}
+
+				num0 = num0 + num1;
+				System.out.println("1년 투자 손익액 : " + num1);
+				System.out.println("잔액은 " + num0 + "원 입니다.");
+
+			} else if (sel == 3) {
+				if (cnt == 2) {
+					num1 = -(num0 / 100) * 36;
+				}
+				if (cnt == 4) {
+					num1 = (num0 / 100) * 75;
+				}
+				num0 = num0 + num1;
+				System.out.println("1년 투자 손익액 : " + num1);
+				System.out.println("잔액은 " + num0 + "원 입니다.");
+
+			} else if (sel == 4) {
+				num0 = num0 + sum;
+				System.out.println("1년 투자 손익액 : " + num1);
+				System.out.println("잔액은 " + num0 + "원 입니다.");
+
+			} else {
+				System.out.println("잘못입력하셨습니다.");
+
+			}
+			cnt++;
+			if (cnt == 6) {
+				System.out.println("종료되었습니다.");
+			} else {
+				System.out.println("계속하시겠습니까?");
+				System.out.println("[1] Yes [2] NO");
+				num2 = sc.nextInt();
+			}
+			if (num2 == 2) {
+				System.out.println("잔액은 " + num0 + "원 입니다.");
+				System.out.println("종료되었습니다!");
+				break;
+			}
+
+		}
+
+	}
+
+	public void investlow() {
+		String url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe"; // DB주소
+		String db_id = "campus_e_0516_2"; // db_id
+		String db_pw = "smhrd2"; // db_pw
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			try {
+				conn = DriverManager.getConnection(url, db_id, db_pw);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Random r = new Random();
+		Scanner sc = new Scanner(System.in);
+		int num0 = 1000000;
+
+		int cnt = 1;
+		int sum = 0;
+		int num2 = 0;
+
+		// 이익률 70% 손해율 -35%
+
+		int num1 = ((num0 / 100) * r.nextInt(95) - 35);
+		while (cnt <= 5) {
+			// 알고리즘
+			if (cnt == 2 || cnt == 4) {
+				System.out.println("정보를 구매하시겠습니까?");
+				System.out.println("[1] YES(10만원 차감)  [2] NO");
+				int buy = sc.nextInt();
+				if (buy == 1) {
+					num0 = num0 - 100000;
+				}
+			}
+
+			num1 = ((num0 / 100) * r.nextInt(95) - 35);
+			System.out.println(cnt + ". 투자할 곳을 선택하세요!");
+			System.out.println("[1] 삼성전자 [2] 셀트리온 [3] 카카오 [4] 아무것도 선택하지 않는다(원금유지)");
+			int sel = sc.nextInt();
+			if (sel == 1) {
+				if (cnt == 2) {
+					num1 = ((num0 / 100) * 50);
+				}
+				if (cnt == 4) {
+					num1 = -(num0 / 100) * 30;
+				}
+				num0 = num0 + num1;
+				System.out.println("1년 투자 손익액 : " + num1);
+				System.out.println("잔액은 " + num0 + "입니다.");
+
+			} else if (sel == 2) {
+				if (cnt == 2) {
+					num1 = -((num0 / 100) * 30);
+				}
+				if (cnt == 4) {
+					num1 = -(num0 / 100) * 25;
+				}
+
+				num0 = num0 + num1;
+				System.out.println("1년 투자 손익액 : " + num1);
+				System.out.println("잔액은 " + num0 + "원 입니다.");
+
+			} else if (sel == 3) {
+				if (cnt == 2) {
+					num1 = -(num0 / 100) * 30;
+				}
+				if (cnt == 4) {
+					num1 = (num0 / 100) * 60;
+				}
+				num0 = num0 + num1;
+				System.out.println("1년 투자 손익액 : " + num1);
+				System.out.println("잔액은 " + num0 + "원 입니다.");
+
+			} else if (sel == 4) {
+				num0 = num0 + sum;
+				System.out.println("1년 투자 손익액 : " + num1);
+				System.out.println("잔액은 " + num0 + "원 입니다.");
+
+			} else {
+				System.out.println("잘못입력하셨습니다.");
+
+			}
+			cnt++;
+			if (cnt == 6) {
+				System.out.println("종료되었습니다.");
+			} else {
+				System.out.println("계속하시겠습니까?");
+				System.out.println("[1] Yes [2] NO");
+				num2 = sc.nextInt();
+			}
+			if (num2 == 2) {
+				System.out.println("잔액은 " + num0 + "원 입니다.");
+				System.out.println("종료되었습니다!");
+				break;
+			}
+
+		}
+
+	}
+}
--- Teamgit/src/view/View.java
+++ Teamgit/src/view/View.java
@@ -4,15 +4,18 @@ import java.util.Scanner;
 
 import model.Login;
 import model.Rank;
-
 import model.Join;
 import model.DTO;
+import model.Invest;
 
 public class View { // view 사용자에게 보이기 위한 main 만들기
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("강원뱅크에 오신것을 환영합니다. \n신규 계좌 개설시 투자 초기금 10만원을 지원해드립니다. \n* 지금 바로 가입하세요 *");
+		
+//		invest money = new invest();
+		Invest money = new Invest();
+		System.out.println("강원뱅크에 오신것을 환영합니다. \n신규 계좌 개설시 투자 초기금 100만원을 지원해드립니다. \n* 지금 바로 가입하세요 *");
 
 		while(true) {
 			System.out.println("[1]회원가입 [2]로그인 [3] 종료");
@@ -47,9 +50,9 @@ public class View { // view 사용자에게 보이기 위한 main 만들기
 				DTO dto = new DTO(user_id, user_pw);
 				log.login(dto);
 				
-//				if() {
+// 			    if() {
 //					// 로그인이 성공 게임으로 진행
-//					break;
+					break;
 //				}
 
 			} else if (menu == 3) {
@@ -57,6 +60,21 @@ public class View { // view 사용자에게 보이기 위한 main 만들기
 				break;
 			}
 		}
+		
+		
+		System.out.println("난이도를 설정하세요!");
+		System.out.println("[1] 쉬움(손익률 변동 작음) [2] 보통(손익률 변동 보통) [3] 어려움(손익률 변동 큼)");
+	    int select = sc.nextInt();
+	    if(select == 1) {
+	    	money.investlow();
+	    }else if( select == 2) {
+	    	money.investmid();
+	    } else if (select == 3) {
+	    	money.investhigh();
+	    } else {
+	    	System.out.println("잘못 입력하셨습니다!");
+	    }
+		
 
 
 		
